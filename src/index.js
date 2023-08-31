1fd3f356ef2eb6e603ff1cdc70f607094d5d2d56
Muhammad Akbar <muhammad.akbar5999@gmail.com>
2023-08-31 16:39:38 +0700

    tes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    status: 200,
+    message: "Hello World",
+  });
 });
 
 app.listen(4000, () => {
